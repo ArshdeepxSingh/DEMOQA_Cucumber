fd0ca73467d36509c23d6ca653c07134e7d98ba2
Arshdeep <arshu19091@gmail.com>
2025-03-21 17:35:20 +0530

    Task completed

--- features/automation.feature
+++ features/automation.feature
@@ -1,7 +1,23 @@
 Feature: Web Form Testing
 
   Scenario: Fill the form with valid and invalid inputs
-    Given I open the form page
-    When I enter firstname and lastname in the First Name field
+    Given Login page is opened
+    When I enter <username> and <password>
     When clicks on submit button
-    Then I wait for 5 seconds before closing
+    Then I should see a flash message saying <message>
+
+    Examples:
+      | username      | password          | message                   |
+      | incorrectUser | Password123       | Your username is invalid! |
+      | student       | incorrectPassword | Your password is invalid! |
+
+  Scenario: Positive LogIn test
+    Given Login page is opened
+    When I enter <username> and <password>
+    When clicks on submit button
+    Then I should see a success message
+    Then the logout button should be displayed
+
+    Examples:
+      | username | password    |
+      | student  | Password123 |
--- features/step-definitions/automation_steps.js
+++ features/step-definitions/automation_steps.js
@@ -1,20 +1,29 @@
 const { Given, When, Then } = require("@wdio/cucumber-framework");
 
-Given(/^I open the form page$/, async () => {
-  await browser.url("https://demoqa.com/automation-practice-form");
+Given(/^Login page is opened$/, async () => {
+  await browser.url("https://practicetestautomation.com/practice-test-login/");
 });
 
-When(/^I enter firstname and lastname in the First Name field$/, async () => {
-  await $("#firstName").scrollIntoView();
-  await $("#firstName").setValue("John");
-  await $("#lastName").setValue("Doe");
+When(/^I enter (.*) and (.*)$/, async (username, password) => {
+  await $("#username").setValue(username);
+  await $("#password").setValue(password);
 });
 
 When(/^clicks on submit button$/, async () => {
-  await $('button[type="submit"]').scrollIntoView();
-  await $('button[type="submit"]').click();
+  await $("#submit").click();
 });
 
-Then("I wait for 5 seconds before closing", async () => {
-  await browser.pause(5000);
+Then(/^I should see a flash message saying (.*)$/, async (message) => {
+  await expect($("#error")).toHaveText(message);
+});
+
+Then(/^I should see a success message$/, async () => {
+  const successMessage = await $(".post-title"); 
+  await successMessage.waitForExist({ timeout: 5000 }); 
+  await expect(successMessage).toHaveText("Logged In Successfully"); 
+});
+
+Then(/^the logout button should be displayed$/, async () => {
+  await expect($(".wp-block-button__link")).toBeDisplayed();
+  await $(".wp-block-button__link").click();
 });
